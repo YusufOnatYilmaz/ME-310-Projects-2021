deb802faa9911bfad2ecdfef74512f202b734d18
YusufOnatYilmaz <76898984+YusufOnatYilmaz@users.noreply.github.com>
2022-02-08 22:55:27 +0300

    Add files via upload

---
+++ df.py
@@ -0,0 +1,13 @@
+def df(x):
+    try:
+        #value = math.log(3)* (3**x)
+        #value = -1 / (x**2)
+        #value = math.cos(math.degrees(x))
+        value = -2*x
+
+    except:
+        print("df funciton is not defined properly. Please enter a correct function.")
+        raise
+
+    return value
+
